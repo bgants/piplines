e85cbfef4cd40bc34b0a3ee71846e39886125a76
rgants <Robert.Gants@solers.com>
2020-03-05 13:00:24 -0500

    adding some testing files.

--- other-pipeline/testIt.groovy
+++ other-pipeline/testIt.groovy
@@ -2,11 +2,10 @@ def testMethod() {
     println( "hi there from groovy files");
 }
 
-def notifier(emailAddreses) {
+def notifier(emailAddreses,buildUrl,buildResult) {
   mail to: 'robert.gants@solers.com, kaleigh.rosenburg@solers.com',
   subject: "status of pipeline: ${currentBuild.fullDisplayName}",
-  body: "${env.BUILD_URL} has result ${currentBuild.result}"
+  body: "${buildUrl} has result ${buildResult}"
 }
-
 
 return this
